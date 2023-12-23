1051aefa67840d4db9e198487a8da1cc6654942d
Brian Morearty <brian@morearty.org>
2023-12-22 20:21:46 -0800

    more

--- 01-naughty-or-nice/src/routes/+page.svelte
+++ 01-naughty-or-nice/src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Child } from './types';
+	import ShowChild from './ShowChild.svelte';
+
+	let children: Child[];
+	let errorMessage: string;
+	let name: string;
+	$: niceChildren = children?.filter((child) => child.tally >= 0).sort((a, b) => b.tally - a.tally);
+	$: naughtyChildren = children
+		?.filter((child) => child.tally < 0)
+		.sort((a, b) => a.tally - b.tally);
+
+	fetch('https://advent.sveltesociety.dev/data/2023/day-one.json')
+		.then((response) => response.json())
+		.then((json) => {
+			children = json;
+		})
+		.catch(() => (errorMessage = 'Unable to get naughty or nice data!'));
+
+	function naughtyOrNice(list: Child[], i: number, offset: number) {
+		list[i].tally += offset;
+		niceChildren = niceChildren;
+		naughtyChildren = naughtyChildren;
+	}
+
+	function addChild() {
+		children = [{ name, tally: 0 }, ...children];
+	}
+</script>
+
+<h1>Naughty or nice counter</h1>
+
+{#if errorMessage}
+	<p class="error">{errorMessage}</p>
+{/if}
+{#if children}
+	<form action="GET" on:submit|preventDefault={addChild}>
+		<input type="text" placeholder="Name" bind:value={name} name="name" />
+		<input type="submit" value="Add a child" />
+	</form>
+	<table>
+		<tr>
+			<td style="vertical-align: top">
+				<table class="nice">
+					{#each niceChildren as child, i}
+						<ShowChild {child} list={niceChildren} onclick={naughtyOrNice} {i} />
+					{/each}
+				</table>
+			</td>
+			<td style="vertical-align: top">
+				<table class="naughty">
+					{#each naughtyChildren as child, i}
+						<ShowChild {child} list={naughtyChildren} onclick={naughtyOrNice} {i} />
+					{/each}
+				</table></td
+			>
+		</tr>
+	</table>
+{/if}
+
+<style>
+	.error {
+		color: red;
+	}
+	.nice {
+		background-color: greenyellow;
+	}
+	.naughty {
+		background-color: hotpink;
+	}
+</style>
---
+++ 01-naughty-or-nice/src/routes/ShowChild.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Child } from './types';
+
+	export let list: Child[];
+	export let child: Child;
+	export let i: number;
+	export let onclick: (list: Child[], i: number, offset: number) => void = () => {};
+</script>
+
+<tr>
+	<td>{child.name}</td>
+	<td>{child.tally}</td>
+	<td><button on:click={() => onclick(list, i, -1)}>Naughtier</button></td>
+	<td><button on:click={() => onclick(list, i, 1)}>Nicer</button></td>
+</tr>
