f685a4ba3dac3c93c0a34fdf9f2cdefdccdd1bbf
Brian Morearty <brian@morearty.org>
2023-12-22 20:51:09 -0800

    more

--- 01-naughty-or-nice/src/routes/+page.svelte
+++ 01-naughty-or-nice/src/routes/+page.svelte
@@ -1,26 +1,44 @@
 <script lang="ts">
 	import type { Child } from './types';
 	import ShowChild from './ShowChild.svelte';
+	import { onMount } from 'svelte';
 
 	let children: Child[];
+	let niceChildren: Child[];
+	let naughtyChildren: Child[];
 	let errorMessage: string;
 	let name: string;
-	$: niceChildren = children?.filter((child) => child.tally >= 0).sort((a, b) => b.tally - a.tally);
-	$: naughtyChildren = children
-		?.filter((child) => child.tally < 0)
-		.sort((a, b) => a.tally - b.tally);
+	let timeout: number;
 
-	fetch('https://advent.sveltesociety.dev/data/2023/day-one.json')
-		.then((response) => response.json())
-		.then((json) => {
-			children = json;
-		})
-		.catch(() => (errorMessage = 'Unable to get naughty or nice data!'));
+	function updateChildren() {
+		timeout = 0;
+		niceChildren = children?.filter((child) => child.tally >= 0).sort((a, b) => b.tally - a.tally);
+		naughtyChildren = children
+			?.filter((child) => child.tally < 0)
+			.sort((a, b) => a.tally - b.tally);
+	}
+
+	onMount(() => {
+		fetch('https://advent.sveltesociety.dev/data/2023/day-one.json')
+			.then((response) => response.json())
+			.then((json) => {
+				children = json;
+				updateChildren();
+			})
+			.catch(() => (errorMessage = 'Unable to get naughty or nice data!'));
+
+		updateChildren();
+	});
 
 	function naughtyOrNice(list: Child[], i: number, offset: number) {
 		list[i].tally += offset;
 		niceChildren = niceChildren;
 		naughtyChildren = naughtyChildren;
+
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(updateChildren, 1000);
 	}
 
 	function addChild() {
